d9bc50d3f75267eaf7f044133175bd102617eec2
mdchia <u5494149@anu.edu.au>
2019-10-23 12:44:54 +1100

    fix paths and logs

--- twitterbot_regular.sh
+++ twitterbot_regular.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+n=0
 echo Starting tweetbot monitor ...
 while sleep 60; do
-  scrot /path/latest.jpg
+  scrot -q 90 /home/minion/bin/twitterImgBot/latest_screenshot/latest.jpg
   if (($n % 60)); then
     # normal
-    echo Screenshot update at $(date "+%Y-%m-%d %H:%M:%S")
-    python twitterbot.py
+    echo Screenshot update $n at $(date "+%Y-%m-%d %H:%M:%S")
+    python3 twitterbot.py
   else
     # every hour
-    echo Attempting tweet at $(date "+%Y-%m-%d %H:%M:%S")
-    python twitterbot.py --tweet
+    echo Attempting tweet at loop $n at $(date "+%Y-%m-%d %H:%M:%S")
+    python3 twitterbot.py --tweet
   fi
   ((n++))
 done
